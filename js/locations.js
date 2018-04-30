1eeba148be539e37b88eee0e18c1e6bf9cc35f86
DESKTOP-78IFV31\Ron <rjl_8@yahoo.com>
2018-04-30 08:39:40 -0500

    more work

---
+++ js/locations.js
@@ -0,0 +1,62 @@
+var locations = [
+    {
+      title: 'The Alamo',
+      googleAddress: '',
+      wikipediaTitle: 'Alamo Mission in San Antonio',
+      location: {lat: 29.425733, lng: -98.486302}
+    },
+    {
+      title: 'Riverwalk', 
+      googleAddress: '',
+      wikipediaTitle: '',
+      location: {lat: 29.423756, lng: -98.484649}
+    },
+    {
+      title: 'Tower of the Americas', 
+      googleAddress: '',
+      wikipediaTitle: '',
+      location: {lat: 29.418960, lng: -98.483591}
+    },
+    {
+      title: 'Institute of Texan Cultures', 
+      googleAddress: '',
+      wikipediaTitle: '',
+      location: {lat: 29.417133, lng: -98.482285}
+    },
+    {
+      title: 'Alamodome', 
+      googleAddress: '',
+      wikipediaTitle: '',
+      location: {lat: 29.418041, lng: -98.478902}
+    },
+    {
+      title: 'Tobin Center', 
+      googleAddress: '',
+      wikipediaTitle: '',
+      location: {lat: 29.430294, lng: -98.488764}
+    },
+    {
+      title: 'Main Plaza', 
+      googleAddress: '',
+      wikipediaTitle: '',
+      location: {lat: 29.424581, lng: -98.493355}
+    },
+    {
+      title: 'El Mercado', 
+      googleAddress: '',
+      wikipediaTitle: '',
+      location: {lat: 29.425208, lng: -98.500125}
+    },
+    {
+      title: 'Blue Star', 
+      googleAddress: '',
+      wikipediaTitle: '',
+      location: {lat: 29.410034, lng: -98.495619}
+    },
+    {
+      title: 'Veterans Memorial Park',
+      googleAddress: '',
+      wikipediaTitle: '',
+      location: {lat: 29.429615, lng: -98.488741}
+    }
+  ];
