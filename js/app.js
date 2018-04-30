9dc76407d4522592c4ae20ff439f800830f3e80c
DESKTOP-78IFV31\Ron <rjl_8@yahoo.com>
2018-04-30 10:58:18 -0500

    trying to separate out the map functions from the ko model, as per requirements

--- js/app.js
+++ js/app.js
@@ -12,20 +12,27 @@
 
         var filter = ko.observable('');
 
-        this.mapLoadStatus = ko.observable('');
-        this.mapLoadStatus.subscribe(function (newValue) {
-            self.initMapCode();
-        });
+//        this.mapLoadStatus = ko.observable('');
+//        this.mapLoadStatus.subscribe(function (newValue) {
+//            self.initMapCode();
+//        });
 
+        // map code
+        // ********
+        var map;
         this.initMapCode = function() {
-            alert('initializing MapCode');
+            // Constructor creates a new map - only center and zoom are required.
+            map = new google.maps.Map(document.getElementById('map'), {
+                center: {lat: 29.426275, lng: -98.492547},
+                zoom: 13
+            });
 
             // Style the markers a bit. This will be our listing marker icon.
-            var defaultIcon = makeMarkerIcon('0091ff');
+            var defaultIcon = self.makeMarkerIcon('0091ff');
 
             // Create a "highlighted location" marker color for when the user
             // mouses over the marker.
-            var highlightedIcon = makeMarkerIcon('FFFF24');
+            var highlightedIcon = self.makeMarkerIcon('FFFF24');
 
             var infoWindow = new google.maps.InfoWindow();
 
@@ -35,7 +42,7 @@
                 var position = locations[i].location;
                 var title = locations[i].title;
                 // Create a marker per location, and put into markers array.
-                var marker = new google.maps.Marker({
+                locations[i].marker = new google.maps.Marker({
                     position: position,
                     title: title,
                     animation: google.maps.Animation.DROP,
@@ -43,7 +50,8 @@
                     id: i
                 });
                 // Push the marker to our array of markers.
-                markers.push(marker);
+//                locations[i].marker = marker;
+//                markers.push(marker);
                 // Create an onclick event to open the large infowindow at each marker.
 /*                
                 marker.addListener('click', function() {
@@ -55,45 +63,46 @@
             self.showListings();
         };
 
-function populateInfoWindow(marker, infowindow) {
-    // Check to make sure the infowindow is not already opened on this marker.
-    if (infowindow.marker != marker) {
-        // Clear the infowindow content to give the streetview time to load.
-        infowindow.setContent('');
-        infowindow.marker = marker;
-        // Make sure the marker property is cleared if the infowindow is closed.
-        infowindow.addListener('closeclick', function() {
-            infowindow.marker = null;
-        });
-        // Open the infowindow on the correct marker.
-        infowindow.open(map, marker);
-    }  
-};
+        this.populateInfoWindow = function(marker, infowindow) {
+            // Check to make sure the infowindow is not already opened on this marker.
+            if (infowindow.marker != marker) {
+                // Clear the infowindow content to give the streetview time to load.
+                infowindow.setContent('');
+                infowindow.marker = marker;
+                // Make sure the marker property is cleared if the infowindow is closed.
+                infowindow.addListener('closeclick', function() {
+                    infowindow.marker = null;
+                });
+                // Open the infowindow on the correct marker.
+                infowindow.open(map, marker);
+            }  
+        };
 
-this.showListings = function() {
-//    var bounds = new google.maps.LatLngBounds();
-    // Extend the boundaries of the map for each marker and display the marker
-    for (var i = 0; i < markers.length; i++) {
-      markers[i].setMap(map);
-//      bounds.extend(markers[i].position);
-    }
-//    map.fitBounds(bounds);
-};
+        this.showListings = function() {
+        //    var bounds = new google.maps.LatLngBounds();
+            // Extend the boundaries of the map for each marker and display the marker
+            for (var i = 0; i < locations.length; i++) {
+                locations[i].marker.setMap(map);
+        //      bounds.extend(markers[i].position);
+            }
+        //    map.fitBounds(bounds);
+        };
 
-// This function takes in a COLOR, and then creates a new marker
-// icon of that color. The icon will be 21 px wide by 34 high, have an origin
-// of 0, 0 and be anchored at 10, 34).
-function makeMarkerIcon(markerColor) {
-    var markerImage = new google.maps.MarkerImage(
-        'http://chart.googleapis.com/chart?chst=d_map_spin&chld=1.15|0|'+ markerColor +
-        '|40|_|%E2%80%A2',
-        new google.maps.Size(21, 34),
-        new google.maps.Point(0, 0),
-        new google.maps.Point(10, 34),
-        new google.maps.Size(21,34));
-    return markerImage;
-};
-
+        // This function takes in a COLOR, and then creates a new marker
+        // icon of that color. The icon will be 21 px wide by 34 high, have an origin
+        // of 0, 0 and be anchored at 10, 34).
+        this.makeMarkerIcon = function(markerColor) {
+            var markerImage = new google.maps.MarkerImage(
+                'http://chart.googleapis.com/chart?chst=d_map_spin&chld=1.15|0|'+ markerColor +
+                '|40|_|%E2%80%A2',
+                new google.maps.Size(21, 34),
+                new google.maps.Point(0, 0),
+                new google.maps.Point(10, 34),
+                new google.maps.Size(21,34));
+            return markerImage;
+        };
+        // end map code
+        // ************
 
 
 
@@ -109,14 +118,16 @@ function makeMarkerIcon(markerColor) {
         this.init = function () {
             alert('in init');
         };
+
+        this.initMapCode();
     };
 
-    var theViewModel = new ViewModel(locations);
+//    var theViewModel = new ViewModel(locations);
 /*    
     theViewModel.mapLoadStatus.subscribe(function (newValue) {
         theViewModel.initMapCode();
     });
 */
-    ko.applyBindings(theViewModel);
+//    ko.applyBindings(theViewModel);
 //    theViewModel.mapLoadStatus('something');
 //}());
--- js/map.js
+++ js/map.js
@@ -1,83 +1,52 @@
-var map;
+var gmap = {
+    map : null,
 
-var markers = [];
+    markers : [],
 
-function initMap() {
-    // Constructor creates a new map - only center and zoom are required.
-    map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat: 29.426275, lng: -98.492547},
-        zoom: 13
-    });
-
-    // Style the markers a bit. This will be our listing marker icon.
-    var defaultIcon = makeMarkerIcon('0091ff');
-
-    // Create a "highlighted location" marker color for when the user
-    // mouses over the marker.
-    var highlightedIcon = makeMarkerIcon('FFFF24');
-
-    var infoWindow = new google.maps.InfoWindow();
-
-    // Set up markers based on locations
-    for (var i = 0; i < locations.length; i++) {
-        // Get the position from the location array.
-        var position = locations[i].location;
-        var title = locations[i].title;
-        // Create a marker per location, and put into markers array.
-        var marker = new google.maps.Marker({
-            position: position,
-            title: title,
-            animation: google.maps.Animation.DROP,
-            icon: defaultIcon,
-            id: i
+    initMap : function() {
+/*        
+        // Constructor creates a new map - only center and zoom are required.
+        map = new google.maps.Map(document.getElementById('map'), {
+            center: {lat: 29.426275, lng: -98.492547},
+            zoom: 13
         });
-        // Push the marker to our array of markers.
-        markers.push(marker);
-        // Create an onclick event to open the large infowindow at each marker.
-        marker.addListener('click', function() {
-            populateInfoWindow(this, infoWindow);
-        });
-    };
 
-//    showListings();
-}
+        // Style the markers a bit. This will be our listing marker icon.
+        var defaultIcon = makeMarkerIcon('0091ff');
 
-function populateInfoWindow(marker, infowindow) {
-    // Check to make sure the infowindow is not already opened on this marker.
-    if (infowindow.marker != marker) {
-        // Clear the infowindow content to give the streetview time to load.
-        infowindow.setContent('');
-        infowindow.marker = marker;
-        // Make sure the marker property is cleared if the infowindow is closed.
-        infowindow.addListener('closeclick', function() {
-            infowindow.marker = null;
-        });
-        // Open the infowindow on the correct marker.
-        infowindow.open(map, marker);
-    }  
-};
+        // Create a "highlighted location" marker color for when the user
+        // mouses over the marker.
+        var highlightedIcon = makeMarkerIcon('FFFF24');
 
-function showListings() {
-//    var bounds = new google.maps.LatLngBounds();
-    // Extend the boundaries of the map for each marker and display the marker
-    for (var i = 0; i < markers.length; i++) {
-      markers[i].setMap(map);
-//      bounds.extend(markers[i].position);
+        var infoWindow = new google.maps.InfoWindow();
+
+        // Set up markers based on locations
+        for (var i = 0; i < locations.length; i++) {
+            // Get the position from the location array.
+            var position = locations[i].location;
+            var title = locations[i].title;
+            // Create a marker per location, and put into markers array.
+            var marker = new google.maps.Marker({
+                position: position,
+                title: title,
+                animation: google.maps.Animation.DROP,
+                icon: defaultIcon,
+                id: i
+            });
+            // Push the marker to our array of markers.
+            markers.push(marker);
+            // Create an onclick event to open the large infowindow at each marker.
+            marker.addListener('click', function() {
+                populateInfoWindow(this, infoWindow);
+            });
+        };
+
+    //    showListings();
+*/    
+
+        var theViewModel = new ViewModel(locations);
+        ko.applyBindings(theViewModel);
     }
-//    map.fitBounds(bounds);
-};
 
-// This function takes in a COLOR, and then creates a new marker
-// icon of that color. The icon will be 21 px wide by 34 high, have an origin
-// of 0, 0 and be anchored at 10, 34).
-function makeMarkerIcon(markerColor) {
-    var markerImage = new google.maps.MarkerImage(
-        'http://chart.googleapis.com/chart?chst=d_map_spin&chld=1.15|0|'+ markerColor +
-        '|40|_|%E2%80%A2',
-        new google.maps.Size(21, 34),
-        new google.maps.Point(0, 0),
-        new google.maps.Point(10, 34),
-        new google.maps.Size(21,34));
-    return markerImage;
-};
 
+}
